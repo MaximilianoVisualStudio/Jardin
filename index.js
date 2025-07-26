7afc415094df54e0477dfeedcf26207770ed2e7d
MaximilianoVisualStudio <142742269+MaximilianoVisualStudio@users.noreply.github.com>
2025-07-26 18:17:20 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,3 @@
+// This file is intentionally left blank.
+// The application entry point has been moved to an inline script
+// in index.html to prevent MIME type errors on static hosting platforms.
