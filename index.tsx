17a180687a3e1bc2215350ac62b47ca7e626e8ff
MaximilianoVisualStudio <142742269+MaximilianoVisualStudio@users.noreply.github.com>
2025-07-26 18:36:39 -0300

    Delete index.tsx

--- index.tsx
+++
@@ -1,3 +0,0 @@
-// This file is intentionally left blank.
-// The application entry point has been moved to an inline script
-// in index.html to prevent MIME type errors on static hosting platforms.
